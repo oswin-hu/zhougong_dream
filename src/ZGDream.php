bce5046041313e5cc0e11d25395107af79a03efd
胡伟涛 <50671163@qq.com>
2022-07-12 15:45:30 +0800

    初始化项目

---
+++ src/ZGDream.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: oswin
+ * Time: 2022/7/12-15:26
+ * Description:
+ * Version: v1.0
+ */
+
+namespace ZGDream;
+
+class ZGDream
+{
+
+    public function __construct()
+    {
+    }
+}
