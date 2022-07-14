610350d5f14c998ca3b5e0076aa24b6cb79629e9
胡伟涛 <50671163@qq.com>
2022-07-14 17:55:16 +0800

    修改composer 配置

--- src/Providers/ZgDreamProvider.php
+++ src/Providers/ZgDreamProvider.php
@@ -14,6 +14,11 @@ use ZGDream\ZGDream;
 class ZgDreamProvider extends ServiceProvider
 {
 
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
     public function boot(): void
     {
         if (class_exists('CreateZgDreamTable')) {
@@ -21,14 +26,30 @@ class ZgDreamProvider extends ServiceProvider
 
             $this->publishes([
                 __DIR__.'/../migrations/create_zg_dream_table.php.stub' => database_path("/migrations/{$timestamp}_create_zg_dream_table.php")
-            ]);
+            ], 'laravel-zg-dream');
         }
     }
 
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register(): void
     {
         $this->app->singleton('ZGDream', function () {
             return new ZGDream();
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return ['ZGDream'];
     }
 }
